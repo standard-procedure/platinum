de9d33f1362adc1332aa1413c1203467ae2c6744
Rahoul Baruah <rahoulb@echodek.co>
2025-07-22 14:12:47 +0000

    Tidied up buttons

--- lib/platinum/button.rb
+++ lib/platinum/button.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Platinum::Button < Platinum::Base
-  def initialize style = :button, type: "button", **attributes
-    @class = [theme.config[style.to_sym], attributes.delete(:class)].compact
-    @attributes = attributes.merge(type: type).merge(class: @class)
+  def initialize style = :primary_button, type: "button", **attributes
+    @class = theme.config[style.to_sym] || theme.button 
+    @attributes = attributes.merge(type: type, class: [@class, attributes.delete(:class)])
   end
 
   def view_template(&)
