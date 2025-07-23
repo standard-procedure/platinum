e862dc737ad9dd66531f542c17238e7fe8c752fd
Rahoul Baruah <rahoulb@echodek.co>
2025-07-23 12:14:25 +0100

    Deal with stretch columns

--- lib/platinum/password_field.rb
+++ lib/platinum/password_field.rb
@@ -9,12 +9,14 @@ class Platinum::PasswordField < Platinum::Base
   end
 
   def view_template
-    div class: "relative w-full", data: {controller: "password-visibility"} do
-      @form.password_field @attribute, **@attributes
-      span class: "absolute inset-y-0 right-2 top-0 grid w-8 place-content-center" do
-        a href: "#", class: theme.inset_button, data: {action: "password-visibility#toggle"} do
-          span(data: {password_visibility_target: "icon"}) { Icon(theme.show_password_icon) }
-          span(class: "hidden", data: {password_visibility_target: "icon"}) { Icon(theme.hide_password_icon) }
+    div class: "flow-root" do
+      div class: "relative w-full", data: {controller: "password-visibility"} do
+        @form.password_field @attribute, **@attributes
+        span class: "absolute inset-y-0 right-2 top-0 grid w-8 place-content-center" do
+          a href: "#", class: theme.inset_button, data: {action: "password-visibility#toggle"} do
+            span(data: {password_visibility_target: "icon"}) { Icon(theme.show_password_icon) }
+            span(class: "hidden", data: {password_visibility_target: "icon"}) { Icon(theme.hide_password_icon) }
+          end
         end
       end
     end
--- lib/platinum/search.rb
+++ lib/platinum/search.rb
@@ -11,19 +11,21 @@ class Platinum::Search < Platinum::Base
   end
 
   def view_template(&)
-    form(**@attributes) do
-      Platinum::Row(wrap: true, gap: 1, justify: "end", items: "start", class: "text-sm") do
-        div class: "relative" do
-          input type: "text", name: @name, value: @value, placeholder: @placeholder, class: theme.input, data: {search_form_target: "input"}, autofocus: true
-          span class: "absolute inset-y-0 right-2 top-0 grid w-8 place-content-center" do
-            a(href: "#", data: {action: "search-form#submit"}, class: theme.inset_button) { Icon(theme.search_icon) }
+    div class: "flow-root" do
+      form(**@attributes) do
+        Platinum::Row(wrap: true, gap: 1, justify: "end", items: "start", class: "text-sm") do
+          div class: "relative" do
+            input type: "text", name: @name, value: @value, placeholder: @placeholder, class: theme.input, data: {search_form_target: "input"}, autofocus: true
+            span class: "absolute inset-y-0 right-2 top-0 grid w-8 place-content-center" do
+              a(href: "#", data: {action: "search-form#submit"}, class: theme.inset_button) { Icon(theme.search_icon) }
+            end
+            span class: "absolute inset-y-0 right-10 top-0 grid w-8 place-content-center" do
+              a(href: "#", data: {action: "search-form#clearAndSubmit"}, class: theme.inset_danger_button) { Icon(theme.clear_icon) }
+            end
           end
-          span class: "absolute inset-y-0 right-10 top-0 grid w-8 place-content-center" do
-            a(href: "#", data: {action: "search-form#clearAndSubmit"}, class: theme.inset_danger_button) { Icon(theme.clear_icon) }
+          button type: "submit", class: "hidden" do
+            Icon(theme.search_icon)
           end
-        end
-        button type: "submit", class: "hidden" do
-          Icon(theme.search_icon)
         end
       end
     end
