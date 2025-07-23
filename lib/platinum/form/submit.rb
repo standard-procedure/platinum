34ec24d16262e81591dd5d9309dc3348ffbd6410
Rahoul Baruah <rahoulb@echodek.co>
2025-07-23 11:43:56 +0100

    Added form submit control

---
+++ lib/platinum/form/submit.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Platinum::Form::Submit < Platinum::Slotted
+  def initialize text = nil, cancel: true, cancel_url: nil, cancel_text: nil, style: "primary_button"
+    @label = text
+    @cancel = cancel
+    @cancel_url = cancel_url
+    @cancel_text = cancel_text
+    @style = style
+  end
+
+  def label(&contents)
+    @label = contents
+  end
+
+  def view_template(&extras)
+    Row justify: "between" do
+      Row justify: "start", class: "flex-1" do
+        BackLink(url: @cancel_url, text: @cancel_text) if @cancel
+        extras&.call
+      end
+      Row justify: "end", class: "shrink-0" do
+        Button(@style, type: "submit") { button_label }
+      end
+    end
+  end
+
+  private def button_label = @label.respond_to?(:call) ? @label.call : @label.to_s
+end
