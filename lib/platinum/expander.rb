82ae34adc3efe06963fdbd12915d4d5698a82203
Rahoul Baruah <rahoulb@echodek.co>
2025-08-05 17:05:57 +0100

    removed expander text as it looks stupid

--- lib/platinum/expander.rb
+++ lib/platinum/expander.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class Platinum::Expander < Platinum::Base
-  def initialize text: nil, icon: nil, **attributes
+  def initialize icon: nil, **attributes
     @icon = icon || theme.expander_icon
     @attributes = attributes
-    @text = text
   end
 
   def view_template
     details(**mix(class: ["group [&_summary::-webkit-details-marker]:hidden transition duration-500", theme.expander, @attributes.delete(:class)], **@attributes)) do
       summary(class: "flex justify-start") do
         span(class: theme.expander_summary) do
-          Icon @icon, text: @text
+          Icon @icon
         end
       end
       yield
