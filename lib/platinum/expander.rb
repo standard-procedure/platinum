1ecb2d2438f4bdcbe73fa079b0c271ec85491222
Rahoul Baruah <rahoulb@echodek.co>
2025-09-11 11:17:18 +0100

    Added text to expanders again

--- lib/platinum/expander.rb
+++ lib/platinum/expander.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class Platinum::Expander < Platinum::Base
-  def initialize icon: nil, **attributes
+  def initialize icon: nil, text: nil, **attributes
     @icon = icon || theme.expander_icon
     @attributes = attributes
+    @text = text
   end
 
   def view_template
     details(**mix(class: ["group [&_summary::-webkit-details-marker]:hidden transition duration-500", theme.expander, @attributes.delete(:class)], **@attributes)) do
       summary(class: "flex justify-start") do
         span(class: theme.expander_summary) do
-          Icon @icon
+          Icon @icon, text: @text
         end
       end
       yield
