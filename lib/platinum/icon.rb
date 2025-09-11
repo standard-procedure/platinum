5d16cb2a182928ee15a9d25705da63f09aac24be
Rahoul Baruah <rahoulb@echodek.co>
2025-09-11 11:32:18 +0100

    Attempt to hide text on expander summaries

--- lib/platinum/icon.rb
+++ lib/platinum/icon.rb
@@ -16,7 +16,7 @@ class Platinum::Icon < Platinum::Base
   private def icon_with_text
     Row(gap: 1, justify: "start", **@options) do
       i(class: @class)
-      span { @text }
+      span(class: "group-open:hidden") { @text }
     end
   end
 
