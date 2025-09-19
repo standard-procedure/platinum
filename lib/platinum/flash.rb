4b396e4513bef63d1e9f2ad759d064dfcc56b640
Rahoul Baruah <rahoulb@echodek.co>
2025-09-19 12:26:22 +0100

    Bugfix on flash

--- lib/platinum/flash.rb
+++ lib/platinum/flash.rb
@@ -4,13 +4,8 @@ class Platinum::Flash < Platinum::Base
   include Phlex::Rails::Helpers::Flash
 
   def view_template(&)
-    span { flash[:alert].to_s }
-    span { flash[:notice].to_s }
-
-    Column do
-      keys.each do |key|
-        Alert(key, text: flash[key].to_s) if flash[key].present?
-      end
+    keys.each do |key|
+      Alert(key, title: flash[key].to_s) if flash[key].present?
     end
   end
 
