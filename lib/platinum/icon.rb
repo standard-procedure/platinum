0c73168a45fc8631252e33576ef2505f0c3f2594
Rahoul Baruah <rahoulb@echodek.co>
2025-09-23 23:00:18 +0100

    Icon wrap

--- lib/platinum/icon.rb
+++ lib/platinum/icon.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Platinum::Icon < Platinum::Base
-  def initialize icon, text: nil, **options
+  def initialize icon, text: nil, wrap: false, **aptions
     @text = text
     @class = [options.delete(:class), lookup(icon)].compact
     @options = options
+    @wrap = wrap ? "text-wrap" : "text-nowrap"
   end
 
   def view_template
@@ -16,7 +17,7 @@ class Platinum::Icon < Platinum::Base
   private def icon_with_text
     Row(gap: 1, justify: "start", **@options) do
       i(class: @class)
-      span(class: "group-open:hidden text-nowrap") { @text }
+      span(class: "group-open:hidden #{@wrap}") { @text }
     end
   end
 
