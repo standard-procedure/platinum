666bbd043d15a71fdc494d669cfdaaa253b554ab
Rahoul Baruah <rahoulb@echodek.co>
2025-09-24 11:59:12 +0100

    Improvements for small screens

--- lib/platinum/breadcrumb.rb
+++ lib/platinum/breadcrumb.rb
@@ -4,7 +4,7 @@ class Platinum::Breadcrumb < Platinum::Base
   def view_template(&contents)
     Platinum::Row(gap: 1) do
       contents&.call
-      Icon(theme.breadcrumb_icon, class: theme.muted)
+      Icon(theme.breadcrumb_icon, class: theme.muted, wrap: false)
     end
   end
 end
--- lib/platinum/row.rb
+++ lib/platinum/row.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Platinum::Row < Platinum::Flex
-  def initialize(gap: 1, justify: :between, items: :center, wrap: false, **attributes)
+  def initialize(gap: 1, justify: :between, items: :center, wrap: true, **attributes)
     direction = wrap ? :wrapped_row : :scrolling_row
     super(direction: direction, gap: gap, justify: justify, items: items, **attributes)
   end
