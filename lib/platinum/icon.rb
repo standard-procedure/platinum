ecff5a3616379e795f1f69694a8e55022433a0af
Rahoul Baruah <rahoulb@echodek.co>
2025-10-23 11:45:11 +0100

    Rows now wrap by default
    So we have to explicitly pass the wrap property in

--- lib/platinum/icon.rb
+++ lib/platinum/icon.rb
@@ -5,7 +5,8 @@ class Platinum::Icon < Platinum::Base
     @text = text
     @class = [options.delete(:class), lookup(icon)].compact
     @options = options
-    @wrap = wrap ? "text-wrap" : "text-nowrap"
+    @wrap = wrap 
+    @text_wrap = @wrap ? "text-wrap" : "text-nowrap"
   end
 
   def view_template
@@ -15,9 +16,9 @@ class Platinum::Icon < Platinum::Base
   private def icon = i(**mix(class: @class, **@options))
 
   private def icon_with_text
-    Row(gap: 1, justify: "start", **@options) do
+    Row(gap: 1, justify: "start", wrap: @wrap, **@options) do
       i(class: @class)
-      span(class: "group-open:hidden #{@wrap}") { @text }
+      span(class: "group-open:hidden #{@text_wrap}") { @text }
     end
   end
 
