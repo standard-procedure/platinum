499ce17e3ba513386721de33afd3959f961243b2
Rahoul Baruah <rahoulb@echodek.co>
2026-01-15 17:16:56 +0000

    Updated ColumnOrRow

--- lib/platinum/column_or_row.rb
+++ lib/platinum/column_or_row.rb
@@ -2,12 +2,11 @@
 
 class Platinum::ColumnOrRow < Platinum::Base
   prop :gap, Integer, default: 1
-  prop :justify, Types.Enum("between", "start", "end", "evenly", "around", "center", "stretch"), default: "between"
-  prop :items, Types.Enum("start", "end", "baseline", "center", "stretch"), default: "center"
-  prop :wrap, _Boolean, default: true
+  prop :column, _Map(justify: Types.Enum("between", "start", "end", "evenly", "around", "center", "stretch"), items: Types.Enum("start", "end", "baseline", "center", "stretch"), wrap: _Boolean), default: {justify: "start", items: "stretch", wrap: false}.freeze
+  prop :row, _Map(justify: Types.Enum("between", "start", "end", "evenly", "around", "center", "stretch"), items: Types.Enum("start", "end", "baseline", "center", "stretch"), wrap: _Boolean), default: {justify: "start", items: "center", wrap: true}.freeze
   prop :attributes, Hash, :**, default: {}.freeze
 
   def view_template(&)
-    div(**mix(class: ["flex", "flex-col", "md:flex-row", "gap-#{@gap}", "justify-#{@justify}", "items-#{@items}", @wrap ? "flex-wrap" : "flex-nowrap", @attributes.delete(:class)].flatten, **@attributes), &)
+    div(**mix(class: ["flex", "flex-col", "gap-#{@gap}", "justify-#{@column[:justify]}", "items-#{@column[:items]}", @column[:wrap] ? "flex-wrap" : "flex-nowrap", "md:flex-row", "md:justify-#{@row[:justify]}", "md:items-#{@row[:items]}", @row[:wrap] ? "md:flex-wrap" : "md:flex-nowrap", @attributes.delete(:class)].flatten, **@attributes), &)
   end
 end
