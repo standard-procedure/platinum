96e1e0a78759dea3a4a086112931a21f96960842
Rahoul Baruah <rahoulb@echodek.co>
2026-01-15 17:35:37 +0000

    Typo

--- lib/platinum/column_or_row.rb
+++ lib/platinum/column_or_row.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Platinum::ColumnOrRow < Platinum::Base
-  prop :size, Types::Size, default: "md"
+  prop :size, Types.Size, default: "md"
   prop :gap, Integer, default: 1
   prop :column, _Map(justify: Types.Enum("between", "start", "end", "evenly", "around", "center", "stretch"), items: Types.Enum("start", "end", "baseline", "center", "stretch"), wrap: _Boolean), default: {justify: "start", items: "stretch", wrap: false}.freeze
   prop :row, _Map(justify: Types.Enum("between", "start", "end", "evenly", "around", "center", "stretch"), items: Types.Enum("start", "end", "baseline", "center", "stretch"), wrap: _Boolean), default: {justify: "start", items: "center", wrap: true}.freeze
